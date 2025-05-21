426e7fdb992141aade1909336dc17801cee10cef
MENDOZA2414 <jmendoza_21@alu.uabcs.mx>
2025-05-21 16:29:10 -0700

    Corrige .gitignore y elimina node_modules y .env del repositorio

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Jm24141002',
-  database: 'nexus_banca'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
 
 module.exports = pool.promise();
